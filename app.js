8365ce79aba321557e35f3d9dce49acdf8b5699b
Maisha Khan <maishasaiyara@gmail.com>
2023-04-01 14:30:10 -0500

    third commit

--- app.js
+++ app.js
@@ -1,223 +1,203 @@
-/*****************STATE******************/
-
-const state = {};
-
-// state.board = ['this is the board']
-// console.log(state)
+/********************STATE********************/
+let state = {};
 
 const resetState = () => {
-
-state.board = [
-  { value: 'X','O', isTurned: false},
-  { value: 'X','O', isTurned: false}, 
-  { value: 'X','O', isTurned: false},
-  { value: 'X','O', isTurned: false},
-  { value: 'X','O', isTurned: false}, 
-  { value: 'X','O', isTurned: false},  
-  { value: 'X','O', isTurned: false},  
-  { value: 'X','O', isTurned: false}, 
-  { value: 'X','O', isTurned: false},  
-];
-state.players = ['',''];
-state.score = [0,0];
-state.lastTurnedIdx = -1;  //Undefined
-
+    state.board = Array(9);
+    state.currentPlayerIdx = -1;
+    state.players = ['', ''];
+    state.lastTurnedIdx = -1;
+    state.pieces = [-1, -2, -3, -4, -5, -6, -7, -8, -9];
+    state.winner = -1;
 };
 
-// When I click on a square I want it to display a word. That wou
-
-
-/*************DOM SELECTORS**************/
+/****************DOM SELECTORS****************/
 const body = document.querySelector('body')
-const board = document.createElement('main');
+let startButton;
 
-
-// console.log('helloooo from app.js!');
-// console.log('body');
-
-//h1 is added by creating an element
-
-// Title element is below
-const titleH1 = document.createElement('h1');
-titleH1.id = 'title';
-titleH1.innterText = 'Tic Tac Toe!';
-
-body.appendChild(titleH1)
-
-function removeChildNodes (parent){
-while parent.firstChild){
-  parent.removeChild(parent.firstChild);
-}
+// Title element below
+function setTitle(theTitle) {
+    const titleH1 = document.createElement('h1');
+    titleH1.id = 'titleH1';
+    titleH1.innerText = theTitle;
+    body.appendChild(titleH1);
 }
 
-//Main element is below
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
+/*******************RENDER FUNCTIONS************/
 const renderBoard = () => {
-  board.id = 'board';
-  // main.innertext = 'this is main';
-    for (let i = 0; i < state.board.length; i++){
-      cnst card = state.board[i];
-      const squareElem = doment.createElement("div");
-      squareElem.className = 'square';    //This adds className of square to each element
-      squareElem.dataset.index = i;     //This adds an index to check against state later
-      
-      if(squareElem.isTurned) squareElem.innerText = card.value;
-      board.appendChild(squareElem);
-}
+    const board = document.createElement('main');
+    removeChildNodes(board);
+    board.id = 'board';
 
-body.appendChild(board);
+    for (let i = 0; i < state.board.length; i++) {
+        const squareElem = document.createElement('div');
+        squareElem.className = 'square';
+        squareElem.dataset.index = i;
+
+        board.appendChild(squareElem);
+    }
+
+    body.appendChild(board);
 };
-
-//Player element is below
-const playerElem = document.createElement
-('p');
 
 const renderPlayer = () => {
-playerElem.id = 'playerNames';
+    const playerElem = document.createElement('p');
 
-let playerElemHTML = '<input placeholder="Enter Player 1 
-  Name"/><input placeholder="Enter Player 2 Name"/> 
-  <button> Game Begins</button>';
+    playerElem.id = 'playerNameElem';
+    
+    let playerElemHTML = '<input type="text" name="player1" placeholder="Enter Player 1 Name" /> <input type="text" name="player2" placeholder="Enter Player 2 Name" /> <button id="start"> Start Game </button>';
 
-playerElem.innterHTML = [playerElemHTML];
+    playerElem.innerHTML = playerElemHTML;
 
-body.appendChild(playerElem);
+    body.appendChild(playerElem);
+    startButton = document.querySelector("#start");
 };
 
-
-function render () {
-  renderBoard();
-  renderPlayer ();
-  console.log('state is: ', state);
-
+const renderScoreElem = () => {
+    let scoreElem = document.createElement('p');
+    scoreElem.innerHTML = `<div>${state.players[0]} : ${state.score[0]}</div><div>${state.players[1]} : ${state.score[1]}</div>`
+    document.querySelector("#titleH1").appendChild(scoreElem);
 }
 
-const takeTurn = (index) => {
-  const card = state.board[index];
-  if(card.isTurned) return;
-  card.isTurned = true;
-  let lastTurnedCard = state.board[state.lastTurnedIdx] = || {};
+/****************EVENT LISTENERS****************/
 
-  if(lastTurnedCard.value === card.value) {
-   // console.log ('they are the same')
-  } else {
-    // lastTurnedCard.isTurned = false;
-   // card.isTurned = false;
-  }
-  // console.log (card);
-};
+function addEvents() {
+    board.addEventListener('click', (event) => {
+        if (!event.target.classList.contains('square')) return;
+        console.log(state.players);
+        let squareIdx = event.target.dataset.index;
+        takeTurn(squareIdx)
+    });
 
-/*************EVENT LISTENERS**************/
+    startButton.addEventListener('click', (event) => {
+        setPlayerNames();
 
-board.addEventListener('click', (event) => {
-  // console.log ('when you click the board');
-  //console.log ('event');
-  //console.log ('event.target');
-  board[0].classList.pop();
+        if(state.players[0] === '' || state.players[1] === '') return;
 
-  if (!event.target.classList.contains('square')) return;   //<-- for lines, or something small you don't want to click on
-  // console.dir(event.target); 
-  // console.dir(event.target.dataset); 
-  // console.log(event.target.dataset); 
-  let squareIdx = event.target.datatset.index
-  takeTurn(squareIdx)
- 
-  // console.log (card);
-  render ();
-});
+        setFirstPlayer();
+        renderTurnElem();
+        document.querySelector("#playerNameElem").remove();
+    });
+}
 
-resetState();
-render ();
+/********************HELPERS********************/
+function setPlayerNames() {
+    state.players[0] = document.querySelector('input[name="player1"]').value;
+    state.players[1] = document.querySelector('input[name="player2"]').value;
+}
 
+function handleEnd(){
+    let turnElem = document.querySelector("#titleH1 p");
+    turnElem.innerHTML = `${state.players[state.currentPlayerIdx]} wins!!`;
 
+    let restartButton = document.createElement("button");
+    restartButton.innerHTML = `<button id="reset"> New Game </button>`
 
+    document.querySelector("#titleH1").append(restartButton);
 
+    restartButton.addEventListener('click', (event) => {
+        console.log("restarted")
+    });
+}
 
+function setFirstPlayer(){
+    state.currentPlayerIdx = getRandomInt(0,1);
+}
 
+const renderTurnElem = () => {
+    let turnElem = document.createElement('p');
+    turnElem.innerHTML = `It's currently ${state.players[state.currentPlayerIdx]}'s turn.`;
+    document.querySelector("#titleH1").appendChild(turnElem);
+}
 
+function updateTurnElem() {
+    let turnElem = document.querySelector("#titleH1 p");
+    turnElem.innerHTML = `It's currently ${state.players[state.currentPlayerIdx]}'s turn.`;
+}
 
+function checkForWin() {
 
-
-
-
-
-
-
-
-
-
-const gameState = {
-    players: ['x', 'o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ]
-  }
-
-
-  let userText = document.getElementById('userText')
-  let restartButton = document.getElementById('restartButton')
-  let squares = Array.from(document.getElementsByClassName('square'))
-
- // console.log(squares)
-
-  const charO = "O"
-  const charX = "X"
-  let presentPlayer = charO
-  let emptySquares = Array (9).fill(null)
-
-  // console.log(emptySquares)
-
-
-  const gameBegins = () => {
-    squares.forEach(square => square.addEventListener('click', squareClicked'))
-  } 
-
-  function squareClicked(b) {
-    // console.log(b.target)
-    const id = b.target.id
-
-    if (!emptySquare[id]){
-      spaces[id] = presentPlayer
-      b.target.innterText = presentPlayer
-
-      if(playerWins()){
-        userMessage = '${presentPlayer}wins!'
-      }
-
-      presentPlayer = presentPlayer == charO   // <-- how do I write changing players?
+    // Loop over rows
+    for (let i = 0; i < 3; i++) {
+        if (state.pieces[3 * i] === state.pieces[(3 * i) + 1] && state.pieces[3 * i] === state.pieces[(3 * i) + 2]) {
+            state.winner = state.currentPlayerIdx;
+            return true;
+        }
     }
-  }
 
-const correctCombos = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [2,4,6],
-  [0,4,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8]
-]
+    // Loop over columns
+    for (let i = 0; i < 3; i++) {
+        if (state.pieces[i] === state.pieces[i + 3] && state.pieces[i] === state.pieces[i + 6]) {
+            state.winner = state.currentPlayerIdx;
+            return true;
+        }
+    }
 
-function playerWins(){
-  for (const condition of playerWins)
+    // Check downward diagonal
+    if (state.pieces[0] === state.pieces[4] && state.pieces[0] === state.pieces[8]) {
+        state.winner = state.currentPlayerIdx;
+        return true;
+    }
+
+    // Check upward diagonal
+    if (state.pieces[2] === state.pieces[4] && state.pieces[2] === state.pieces[6]) {
+        state.winner = state.currentPlayerIdx;
+        return true;
+    }
+
+    return false;
 }
 
-  restartButton.addEventListener('click', restart)
+function takeTurn(squareNum) {
+    if(state.players[0] === '' || state.players[1] === '') return;
+    
+    if (state.winner != -1) return;
 
-  function restart () {
-    emptySquares.fill(null)
+    let eachSquare = document.querySelector('#board').children;
 
-    squares.forEach(square => {
-      square.innerText = ''
-    })
+    if (eachSquare[squareNum].firstChild != null) return;
 
-    userMessage = 'Tic Tac Toe'
+    let playerTurn = document.createElement('p');
 
-    presentPlayer = charO
-  }
+    let playerPiece;
 
+    if (state.currentPlayerIdx === 0) {
+        playerPiece = "⭕";
+        state.pieces[squareNum] = 0;
+        if (checkForWin()) { handleEnd(); }
+        state.currentPlayerIdx = 1;
+    } else {
+        playerPiece = "❌";
+        state.pieces[squareNum] = 1;
+        if (checkForWin()) { handleEnd(); }
+        state.currentPlayerIdx = 0;
+    }
 
-  resetState();  
+    playerTurn.innerText = playerPiece;
+
+    eachSquare[squareNum].appendChild(playerTurn);
+
+    updateTurnElem();
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function main() {
+    setTitle('Tic Tac Toe!');
+
+    resetState();
+    renderBoard();
+    renderPlayer();
+
+    addEvents();
+}
+
+main();
+
